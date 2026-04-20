ab686faad664bd0d02d902b016b9cf6af15614a3
Nima Foroohari <nforoohari@gmail.com>
2026-04-20 19:20:38 +0330

    First Step

--- src/main/java/Algs/Alg1.java
+++ src/main/java/Algs/Alg1.java
@@ -1,8 +1,29 @@
 package Algs;
 
 public class Alg1 {
+
     public static void main(String[] args) {
         System.out.println("Hello Algorithms");
+        buy(CNames.EEE, 1.0F, 2.3F);
+        sell(CNames.TTT, 3.0F, 4.5F);
+
     }
 
+    static void buy(CNames cNames, Float priceLimit, Float volume) {
+//        System.out.println("Buy  , " + "priceLimit : " + priceLimit + " cNames : " + cNames + " , code : " + cNames.getCode() + " , volume : " + volume);
+        buyProxy(cNames, priceLimit, volume);
+    }
+
+    static void sell(CNames cNames, Float priceLimit, Float volume) {
+//        System.out.println("Sell , " + "priceLimit : " + priceLimit + " cNames : " + cNames + " , code : " + cNames.getCode() + " , volume : " + volume);
+        sellProxy(cNames, priceLimit, volume);
+    }
+
+    static void buyProxy(CNames cNames, Float priceLimit, Float volume) {
+        System.out.println("BuyProxy  , " + "priceLimit : " + priceLimit + " cNames : " + cNames + " , code : " + cNames.getCode() + " , volume : " + volume);
+    }
+
+    static void sellProxy(CNames cNames, Float priceLimit, Float volume) {
+        System.out.println("SellProxy , " + "priceLimit : " + priceLimit + " cNames : " + cNames + " , code : " + cNames.getCode() + " , volume : " + volume);
+    }
 }
---
+++ src/main/java/Algs/CNames.java
@@ -0,0 +1,24 @@
+package Algs;
+
+//public enum CNames {
+//    BBB,
+//    EEE,
+//    TTT
+//}
+
+public enum CNames {
+    BBB(1),
+    EEE(2),
+    TTT(3);
+
+    private final int code;
+
+    CNames(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
+
